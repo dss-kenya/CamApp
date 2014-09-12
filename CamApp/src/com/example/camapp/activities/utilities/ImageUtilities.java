afa86bf2d07fef243152aad57e9703f942bbafea
dhara <hearbeat.horror@gmail.com>
2014-09-12 18:05:23 +0530

    updated for file creation

--- CamApp/src/com/example/camapp/activities/utilities/ImageUtilities.java
+++ CamApp/src/com/example/camapp/activities/utilities/ImageUtilities.java
@@ -13,41 +13,45 @@ public class ImageUtilities {
 	private static File mImgFileName;
 	private static final String SAVED_FILE_FOLDER = "SavedImages";
 	private static final String MODIFIED_FILE_FOLDER="SuperImposedImages";
-	
+
 	public static void savePhoto(Bitmap mutableBitmap) {
 		File imageFileFolder = new File(Environment.getExternalStorageDirectory(), 
 				CamApp.getApp().getPackageName() + 
 				"/" +
 				SAVED_FILE_FOLDER);
-        imageFileFolder.mkdirs();
-        FileOutputStream out = null;
-        Calendar c = Calendar.getInstance();
-        
-        String date = fromInt(c.get(Calendar.MONTH)) + 
-        		fromInt(c.get(Calendar.DAY_OF_MONTH)) + 
-        		fromInt(c.get(Calendar.YEAR)) + 
-        		fromInt(c.get(Calendar.HOUR_OF_DAY)) + 
-        		fromInt(c.get(Calendar.MINUTE)) + 
-        		fromInt(c.get(Calendar.SECOND));
-        
-        mImgFileName = new File(imageFileFolder, date.toString() + ".png");
-        
-        if(!mImgFileName.exists()) {
-        	mImgFileName.mkdirs();
-        }
-        
-        try {
-            out = new FileOutputStream(mImgFileName);
-            mutableBitmap.compress(Bitmap.CompressFormat.PNG,100, out);
-            out.flush();
-            out.close();
-            out = null;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+
+		if(!imageFileFolder.exists()) {
+			imageFileFolder.mkdirs();
+		}
+
+		FileOutputStream out = null;
+		Calendar c = Calendar.getInstance();
+
+		String date = fromInt(c.get(Calendar.MONTH)) + 
+				fromInt(c.get(Calendar.DAY_OF_MONTH)) + 
+				fromInt(c.get(Calendar.YEAR)) + 
+				fromInt(c.get(Calendar.HOUR_OF_DAY)) + 
+				fromInt(c.get(Calendar.MINUTE)) + 
+				fromInt(c.get(Calendar.SECOND));
+
+		mImgFileName = new File(imageFileFolder, date.toString() + ".png");
+
+		try {
+			if(!mImgFileName.exists()) {
+				mImgFileName.createNewFile();
+			}
+
+			out = new FileOutputStream(mImgFileName);
+			mutableBitmap.compress(Bitmap.CompressFormat.PNG,100, out);
+			out.flush();
+			out.close();
+			out = null;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
-	
+
 	public static String fromInt(int val) {
-        return String.valueOf(val);
-    }
+		return String.valueOf(val);
+	}
 }
